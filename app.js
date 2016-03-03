6187674afaaa7d1535dc7e0b6034cb1372a4b1ce
Heley Chen <zccz14@outlook.com>
2016-03-03 12:09:31 +0800

    将index挂载到 /

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/',require('./routes/index'));
