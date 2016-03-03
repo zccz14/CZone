abbd89ebd2de3d68518693550c8beb1a4bac6c75
Heley Chen <zccz14@outlook.com>
2016-03-03 11:41:49 +0800

    连接MongoDB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('mongoose').connect('mongodb://localhost/czone');
 var app = express();
 module.exports = app;
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
